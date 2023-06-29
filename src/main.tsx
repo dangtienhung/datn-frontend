e6974b2929b4ff99544a61fec23b9b2dcd7eebc7
danghung <hungdang02042003@gmail.com>
2023-06-29 10:00:36 +0700

    initial

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import './index.scss';
+
+import App from './App.tsx';
+import { Provider } from 'react-redux';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { store } from './store/store.ts';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>
+);
